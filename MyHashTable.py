3bb5082867e7feadd465b283a2aad8f3ed5138e1
shahidhj <shahid.h.j@gmail.com>
2019-09-10 07:12:35 -0700

    added objetcs to class

--- MyHashTable.py
+++ MyHashTable.py
@@ -1,18 +1,19 @@
 class HashTable:
 
     def __init__(self):
-        self.map = []
+        self.size = 8
+        self.map = [None]*self.size
 
     def _get_hash(self,key):
-        hash = 0
-        for char in key:
-            hash = hash + ord(key)
-        return hash % len(self.map)
+        hashed = 0
+        for char in str(key):
+            hashed = hashed + ord(char)
+        return hashed % self.size
 
     def add(self,key,value):
         hash_address = self._get_hash(key)
         key_value = [key,value]
-        if self.map[hash_address] is not None:
+        if self.map[hash_address] is None:
             key_value = list([key,value])
             self.map[hash_address] = key_value
             return True
@@ -39,6 +40,17 @@ class HashTable:
                 if self.map[hash_address][i][0] == key:
                     self.map[hash_address].pop(i)
                     return True
+
+    def print(self):
+        for eachItem in self.map:
+            if eachItem is not None:
+                print(str(eachItem))
+
+myFirstHash = HashTable()
+myFirstHash.add("shahid","1234")
+myFirstHash.add("abc","3214")
+myFirstHash.print()
+print(myFirstHash.get("abc"))
 
 
 
