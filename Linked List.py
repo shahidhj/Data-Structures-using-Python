196d0402c83079fadb1b66001f3095b798128c83
shahidhj <shahid.h.j@gmail.com>
2019-09-15 14:43:09 -0700

    Implementation of linked list

---
+++ Linked List.py
@@ -0,0 +1,38 @@
+class LinkedList:
+    def __init__(self,value):
+        self.head = {
+            value:value,
+            next: None
+        }
+        self.tail = self.head
+        self.length = 1
+        self.NewNode = {}
+
+    def append(self,value):
+        self.NewNode = {
+            value:value,
+            next:None
+        }
+        self.tail[next] = self.NewNode
+        self.tail = self.NewNode
+        self.length = self.length + 1
+
+    def prepend(self,value):
+        self.PrependNode = {
+            value:value,
+            next :None
+        }
+        self.PrependNode[next]= self.head
+        self.head = self.PrependNode
+        self.length = self.length +1
+
+    def insert(self,index,value):
+
+
+myLinkedList = LinkedList(10)
+myLinkedList.append(12)
+myLinkedList.append(4)
+myLinkedList.append((3))
+myLinkedList.prepend(14)
+myLinkedList.prepend(190)
+print(myLinkedList.head)
