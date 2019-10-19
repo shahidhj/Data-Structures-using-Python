fc52b85078ebe340457e136a511cdced59a934cd
shahidhj <shahid.h.j@gmail.com>
2019-10-18 23:19:13 -0700

    Binary Search Tree

--- BinarySearchTrees.py
+++ BinarySearchTrees.py
@@ -26,3 +26,48 @@ class BinarySearchTree:
                         currentNode.left = currentNode
                         return self
                     currentNode = currentNode.left
+
+    def lookup(self,value):
+        if(self.root == None):
+            return False
+        else:
+            currentNode = self.root
+            while currentNode is not None:
+                if value < currentNode.value:
+                    currentNode = currentNode.left
+                elif value > currentNode.value:
+                    currentNode = currentNode.right
+                elif value == currentNode.value :
+                    return currentNode
+        return False
+    """
+    def remove(self,value):
+        if self.root is None:
+            return False
+        else:
+            currentNode = self.root
+            previousNode = None
+            while currentNode is not None:
+                if value < currentNode.value:
+                    previousNode = currentNode
+                    currentNode = currentNode.left
+                elif value > currentNode.value:
+                    previousNode = currentNode
+                    currentNode = currentNode.right
+                elif value == currentNode.value :
+                     if(currentNode.right):
+                         currentNode = currentNode.right
+                         if currentNode.left:
+                             currentNode = currentNode.left
+                             previousNode.right = currentNode
+                         else:
+                             previousNode.right = currentNode
+                     else:
+                         currentNode = currentNode.left
+                         previousNode
+                         
+        Too complicated:("""
+
+
+
+
